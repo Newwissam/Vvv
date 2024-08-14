83fa1493fcfbfd5ab92735b6c0653ef7bcf9fa61
bblkt <169025378+bblkt@users.noreply.github.com>
2024-08-14 13:25:38 +0300

    Update bot2.py

--- YousefMusic/plugins/play/bot2.py
+++ YousefMusic/plugins/play/bot2.py
@@ -12,29 +12,22 @@ bot_name = {}
 
 name = "بوت"
 
-@app.on_message(filters.regex("تعيين اسم البوت")& filters.private & SUDOERS, group=7113)
-async def set_bot_name(client, message):
-    global name
-    ask = await app.ask(message.chat.id, "ارسل الاسم الجديد", timeout=300)
-    name = ask.text
-    await message.reply_text("تم تعيين الاسم بنجاح")
-
 Mazen_responses = [
-    "انا بوت خاص ب تشغيل الميوزك ،",
+    "انا بوت خاص ب تشغيل الميوزك   ،",
     "امر كيف يمكنني مساعدتك ⋅",
     "نعم يا عيوني  ،",
     "امرني انا بوت تشغيل الميوزك "
     "ارحب انابوت خاص بالموسيقه",
     " ياهلا امرني كيف يمكنني مساعدتك",
-    "من خلالي يمكنك تشغيل الميوزك",
+    "من خلالي يمكنك تشغيل الميوزك  ",
 ]
 
 @app.on_message(filters.command(["بوت", "ديا"], ""), group=71135)
 async def Mazen_bot(client, message):
-    global name
+    
     bot_username = (await app.get_me()).username
-    bar = random.choice(Mazen_responses).format(name=name)
+    bar = random.choice(Mazen_responses).format(name)
     await message.reply_text(
-        text=f"**{bar}**",
+        text=f"{bar}",
         disable_web_page_preview=True,
     parse_mode=enums.ParseMode.MARKDOWN)
